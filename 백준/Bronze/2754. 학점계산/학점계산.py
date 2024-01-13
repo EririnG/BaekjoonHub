84afe797927bb6025fa5e03f93b57da2e0a698e1
백강민 <78525107+EririnG@users.noreply.github.com>
2024-01-14 00:13:31 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+ans = input()
+
+if ans == 'A+':
+    print(4.3)
+elif ans == 'A0':
+    print(4.0)
+elif ans == 'A-':
+    print(3.7)
+elif ans == 'B+':
+    print(3.3)
+elif ans == 'B0':
+    print(3.0)
+elif ans == 'B-':
+    print(2.7)
+elif ans == 'C+':
+    print(2.3)
+elif ans == 'C0':
+    print(2.0)
+elif ans == 'C-':
+    print(1.7)
+elif ans == 'D+':
+    print(1.3)
+elif ans == 'D0':
+    print(1.0)
+elif ans == 'D-':
+    print(0.7)
+else:
+    print(0.0)
