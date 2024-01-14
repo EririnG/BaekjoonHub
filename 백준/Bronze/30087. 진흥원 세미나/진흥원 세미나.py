1a5d253f426fda47c418e8bb63c5caa78f043ae0
백강민 <78525107+EririnG@users.noreply.github.com>
2024-01-14 14:59:18 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,18 @@
+test_case = int(input())
+
+for i in range(test_case):
+    semi = input()
+    if semi == 'Algorithm':
+        print('204')
+    elif semi == 'DataAnalysis':
+        print('207')
+    elif semi == 'ArtificialIntelligence':
+        print('302')
+    elif semi == 'CyberSecurity':
+        print('B101')
+    elif semi == 'Network':
+        print('303')
+    elif semi == 'Startup':
+        print('501')
+    else:
+        print('105')
