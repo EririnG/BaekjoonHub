4da867c9d381d7c522dc01b71be05c1a5d2ada7e
백강민 <78525107+EririnG@users.noreply.github.com>
2024-01-31 14:50:07 +0900

    [Gold V] Title: LCS, Time: 516 ms, Memory: 56548 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,17 +1,17 @@
 import sys
 
-first_string = list(map(str,sys.stdin.readline().strip()))
+first_string = list(map(str,sys.stdin.readline().rstrip()))
 second_string = list(map(str,sys.stdin.readline().rstrip()))
-first_string.insert(0,'1')
-second_string.insert(0,'2')
-lcs_arr = [[0 for i in range(len(second_string))]for i in range(len(first_string))]
 
+lcs = [[0 for i in range(len(second_string)+1)]for i in range(len(first_string)+1)]
+first_string.insert(0,0)
+second_string.insert(0,0)
 
 for i in range(1,len(first_string)):
     for j in range(1,len(second_string)):
         if first_string[i] == second_string[j]:
-            lcs_arr[i][j] = lcs_arr[i-1][j-1] + 1
-        elif first_string[i] != second_string[j]:
-            lcs_arr[i][j] = max(lcs_arr[i-1][j],lcs_arr[i][j-1])
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i-1][j],lcs[i][j-1])
 
-print(lcs_arr[-1][-1])
+print(lcs[-1][-1])
