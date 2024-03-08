58a87d62cf175203b2c62cfd67f959a2bea39beb
백강민 <78525107+EririnG@users.noreply.github.com>
2024-03-08 10:28:58 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+//백준 10987번 코드
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	string st;
+	cin >> st;
+
+	int count = 0;
+	for (int i = 0; i < st.length(); i++) {
+		if (st[i] == 'a' || st[i] == 'e' || st[i] == 'i' || st[i] == 'o' || st[i] == 'u') {
+			count++;
+		}
+	}
+	cout << count << endl;
+
+}
