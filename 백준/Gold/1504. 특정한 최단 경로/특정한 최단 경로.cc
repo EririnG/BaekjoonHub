ddc5016de0c5525d6b5f82a027c93934b874c71c
에린 <78525107+EririnG@users.noreply.github.com>
2024-03-26 16:59:41 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 148 ms, Memory: 5612 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,8 +1,6 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
+
 int INF = 98765432;
 vector<pair<int, int>> v[802];
 int dist[803];
@@ -17,56 +15,59 @@ void bfs(int a)
         int sum_distance = q.top().first;
         int x = q.top().second;
         q.pop();
-        
-        if(dist[x] < sum_distance)
+
+        if (dist[x] < sum_distance)
             continue;
-        for (int i = 0; i < v[x].size(); i++){
+        for (int i = 0; i < v[x].size(); i++) {
             int nx = v[x][i].first;
             int ncost = sum_distance + v[x][i].second;
- 
-            if(dist[nx] > ncost){
-                q.push({ncost, nx});
+
+            if (dist[nx] > ncost) {
+                q.push({ ncost, nx });
                 dist[nx] = ncost;
             }
         }
     }
 }
-int mn(int a, int b){
-    if(a<b)
+
+int compare_ab(int a, int b)
+{
+    if (a < b)
         return a;
     else
         return b;
 }
-int main(){
-    int n, e;
-    cin >> n >> e;
-    int a1,a2,a3;
-    for (int i = 0; i < e; i++){
-        cin >> a1 >> a2 >> a3;
-        v[a1].push_back({a2, a3});
-        v[a2].push_back({a1, a3});
+
+int main() 
+{
+    int node, edge, result;
+    cin >> node >> edge;
+    for (int i = 0; i < edge; ++i)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v[a].push_back({ b,c });
+        v[b].push_back({ a,c });
     }
-    int dt1, dt2;
-    cin >> dt1 >> dt2;
+    int dest1, dest2;
+    cin >> dest1 >> dest2;
     
     bfs(1);
-    int To_dt1 = dist[dt1];
-    int To_dt2 = dist[dt2];
- 
-    bfs(dt1);
-    int dt1_To_dt2 = dist[dt2];
-    int dt1_To_n = dist[n];
- 
-    bfs(dt2);
-    int dt2_To_n = dist[n];
- 
-    int result;
- 
-    result = mn(INF, To_dt1 + dt1_To_dt2 + dt2_To_n);
-    result = mn(result, To_dt2 + dt1_To_dt2 + dt1_To_n);
-    if(result >= INF)
+    int to_dest1 = dist[dest1];
+    int to_dest2 = dist[dest2];
+
+    bfs(dest1);
+    int dest1_to_dest2 = dist[dest2];
+    int dest1_to_node = dist[node];
+
+    bfs(dest2);
+    int dest2_to_node = dist[node];
+
+    result = compare_ab(INF, to_dest1 + dest1_to_dest2 + dest2_to_node);
+    result = compare_ab(result, to_dest2 + dest1_to_dest2 + dest1_to_node);
+    if (result >= INF)
         cout << -1;
     else
         cout << result;
-    return 0;
+
 }
