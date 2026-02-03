5598d6814df1c8a44d4b71b216b99a74bf7ca518
에린 <78525107+EririnG@users.noreply.github.com>
2026-02-04 00:03:16 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string str,rvstr;
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	vector<char> vec_char;
-	string input;
-
-	cin >> input;
-	copy(input.begin(), input.end(), back_inserter(vec_char));
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> str;
+	rvstr = str;
+	reverse(rvstr.begin(),rvstr.end());
 	
-	for (int i = 0; i < vec_char.size(); ++i)
-	{
-		if (vec_char.at(i) == vec_char.at(vec_char.size() - 1 - i))
-			continue;
-		else
-		{
-			cout << "0";
-			return 0;
-		}
-	}
-	cout << "1";
+    if(str == rvstr) cout << 1 << " ";
+    else cout << 0 << " ";
+    
 	return 0;
 }
